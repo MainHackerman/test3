8a62ebdd2fe5359949c4a37256a6c5dd798f4ba9
Ondrej Tinka <ondrej.tinka@gmail.com>
2018-06-20 16:10:45 +0200

    Added Project

---
+++ project.py
@@ -0,0 +1,35 @@
+string = 'a b c d '
+words = 0
+spaces = 0
+chars = 0
+souhlasky = 0
+samohlasky = 0
+prevcahr = ''
+
+a = 'aeiuoy'
+b = 'qwrtpsdfghjklxcvbnm'
+
+for char in string:
+	if char in a:
+		samohlasky += 1
+
+	elif char in b:
+		souhlasky += 1
+
+	elif char == ' ':
+		if prevchar in a+b:
+			words += 1
+		spaces += 1
+	prevchar = char
+
+chars = len(string) - spaces
+
+print(string)
+print('Slova', words)
+print('Mezery', spaces)
+print('Znaky', chars)
+print('Souhlasky', souhlasky)
+print('Samohlasky', samohlasky)
+
+		
+	
